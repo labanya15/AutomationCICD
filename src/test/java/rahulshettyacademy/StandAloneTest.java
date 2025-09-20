cba5d4142d783d532607b630c16520474aae91ee
labanya <labanyahota15@gmail.com>
2025-09-20 17:11:16 +0530

    Updated StandaloneTest file

--- src/test/java/rahulshettyacademy/StandAloneTest.java
+++ src/test/java/rahulshettyacademy/StandAloneTest.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 public class StandAloneTest {
     public static void main(String[] args) {
+	//new comments are added
 
         String productName = "ZARA COAT 3";
         WebDriverManager.chromedriver().setup();
